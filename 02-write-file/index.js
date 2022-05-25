80142472eedb4d53ebc525fb6e99c192fc839036
Aliaksandr Shabanovich <alikshabanovich@gmail.com>
2022-05-26 00:07:59 +0300

    fit: 02-write-file task without settimeout

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,40 +1,24 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const readline = require("readline");
-// const output = fs.createWriteStream(path.join(__dirname, 'destination.txt'));
-let filePath = path.join(__dirname, 'destination.txt');
+const { stdin, stdout } = process;
+let filePath = path.join(__dirname, "destination.txt");
 
-const writableStream = fs.createWriteStream(filePath);
-
-writableStream.on("error", (error) => {
-  console.log(
-    `An error occured while writing to the file. Error: ${error.message}`
-  );
-});
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: "Enter a sentence: ",
-});
-
-rl.on("line", (line) => {
-  switch (line.trim()) {
-    case "exit":
-      rl.close();
-      break;
-    default:
-      rl.prompt();
-      sentence = line + "\n";
-      writableStream.write(sentence);
-      break;
-  }
-}).on("close", () => {
-  writableStream.end();
-  writableStream.on("finish", () => {
-    console.log(`All your sentences have been written to ${filePath}`);
+fs.writeFile(filePath, "", (err) => {
+  if (err) throw err;
+  stdout.write("Enter a sentence: \n");
+  stdin.on("data", (data) => {
+    if (data.toString().trim() === "exit") {
+      exit();
+    } else {
+      fs.appendFile(filePath, data, (err) => {
+        if (err) throw err;
+      });
+    }
   });
-  setTimeout(() => {
-    process.exit(0);
-  }, 100);
+  process.on("SIGINT", process.exit);
+  process.on("exit", () => {
+    stdout.write(`All your sentences have been written to ${filePath}`);
+    process.exit();
+  });
 });
